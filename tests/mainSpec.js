b7d0b137bc9f36ae5515dc030bd1f9ff30090164
eugenesqr <eskurihin@gmail.com>
2018-03-19 14:56:13 +0300

    removed public masks

--- tests/mainSpec.js
+++ tests/mainSpec.js
@@ -1,3 +1,5 @@
-it("initial test", function () {
-    console.log(dateFormat);
+var datetime = new Date(2018, 03 - 1, 19, 14, 20, 31, 123);
+
+it("default mask", function () {
+    expect(dateFormat(datetime)).toBe("Mon Mar 19 2018 14:20:31");
 });
